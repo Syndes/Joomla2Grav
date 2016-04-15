4993305daa9bd7c884128ec1ab762439c05e4f6e
ocean123 <ocean1@iname.com>
2016-04-15 14:03:03 +0200

    Update markdown.php

--- jm2grav/markdown.php
+++ jm2grav/markdown.php
@@ -51,7 +51,7 @@ if($is_admin) {
 
 			if (!$nr['state']) {$published = 'false';} else {$published = 'true';}
 			$title = $nr['title'];
-			$fileName = $nr['title']; 
+			$fileName = 'default'; 
 			$created = $nr['created'];
 			$metadesc = $nr['metadesc'];
 			$metakey = $nr['metakey'];
